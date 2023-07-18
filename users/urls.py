a295cefc20742a8975fc14b0a46255106aa01c3f
JohnEf <eustathiou.ioan@protonmail.com>
2023-07-18 14:12:21 +0200

    add sign out and token refresh endpoints

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from rest_framework_simplejwt.views import TokenRefreshView
+
 from . import views
 
 
@@ -8,4 +10,6 @@ app_name = 'users'
 urlpatterns = [
     path('signup/', views.SignUpAPIView.as_view(), name="signup"),
     path('signin/', views.SignInAPIView.as_view(), name="signin"),
+    path('sign-out/', views.SignOutAPIView.as_view(), name="sign_out"),
+    path('refresh-token/', TokenRefreshView.as_view(), name="refresh_token"),
 ]
