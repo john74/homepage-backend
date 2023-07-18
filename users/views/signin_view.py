9ed1510e93fe925f06b78554f8f70b5ab648d82d
JohnEf <eustathiou.ioan@protonmail.com>
2023-07-18 14:14:25 +0200

    remove RefreshToken import

--- users/views/signin_view.py
+++ users/views/signin_view.py
@@ -4,7 +4,6 @@ from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework_simplejwt.tokens import RefreshToken
 
 from users.utils import create_jwt_pair_for_user
 
