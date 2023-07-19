8159bc7b60360e76b65ed7ccf2e9432112f422d6
JohnEf <eustathiou.ioan@protonmail.com>
2023-07-19 18:59:09 +0200

    add path to bookmarks app

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/bookmarks/', include('bookmarks.urls', namespace='bookmarks')),
     path('api/users/', include('users.urls', namespace='users')),
 ]
