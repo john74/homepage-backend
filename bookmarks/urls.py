2861dc1f6500b9a60900e34abc64f051c3fee50d
JohnEf <eustathiou.ioan@protonmail.com>
2023-07-20 13:03:56 +0200

    rename endpoints

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -7,8 +7,8 @@ app_name = 'bookmarks'
 
 #bookmark category paths
 urlpatterns = [
-    path('', views.BookmarkCategoryListAPIView.as_view(), name="list"),
-    path('create/', views.BookmarkCategoryBulkCreateAPIView.as_view(), name="create"),
-    path('delete/', views.BookmarkCategoryBulkDeleteAPIView.as_view(), name="delete"),
-    path('update/', views.BookmarkCategoryBulkUpdateAPIView.as_view(), name="update"),
+    path('categories/', views.BookmarkCategoryListAPIView.as_view(), name="list_categories"),
+    path('bulk-create-categories/', views.BookmarkCategoryBulkCreateAPIView.as_view(), name="bulk_create_categories"),
+    path('bulk-delete-categories/', views.BookmarkCategoryBulkDeleteAPIView.as_view(), name="bulk_delete_categories"),
+    path('bulk-update-categories/', views.BookmarkCategoryBulkUpdateAPIView.as_view(), name="bulk_update_categories"),
 ]
