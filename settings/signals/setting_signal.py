82f87916e8431c6b8f648ba9d703cbcfa49c6463
JohnEf <eustathiou.ioan@protonmail.com>
2023-07-24 13:17:22 +0200

    Add handling for non-200 response status codes and KeyError in response JSON data. Instead of attempting to save, return from the function with appropriate error handling.

--- settings/signals/setting_signal.py
+++ settings/signals/setting_signal.py
@@ -8,9 +8,17 @@ from settings.utils import get_location_info
 @receiver(post_save, sender=Setting)
 def populate_setting_fields(sender, **kwargs):
     setting = Setting.objects.first()
-    location_info = get_location_info(setting)
+    response = get_location_info(setting)
 
-    setting.country = location_info['sys']['country']
-    setting.city = location_info['name']
-    setting.timezone = location_info['timezone']
+    if response.status_code != 200:
+        return
+
+    location_info = response.json()
+    try:
+        setting.country = location_info['sys']['country']
+        setting.city = location_info['name']
+        setting.timezone = location_info['timezone']
+    except KeyError:
+        return
+
     setting.save()
