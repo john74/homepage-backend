2d8a7b06320ffb1e6fe66f419998beaa795cd090
JohnEf <eustathiou.ioan@protonmail.com>
2023-07-24 11:37:23 +0200

    create SettingUpdateAPIView

--- settings/urls.py
+++ settings/urls.py
@@ -7,4 +7,5 @@ app_name = 'settings'
 
 urlpatterns = [
     path('create/', views.SettingCreateAPIView.as_view(), name="create"),
+    path('update/', views.SettingUpdateAPIView.as_view(), name="update"),
 ]
---
+++ settings/views/setting_update_view.py
@@ -0,0 +1,21 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from settings.models import Setting
+from settings.serializers import SettingUpdateSerializer
+
+
+class SettingUpdateAPIView(APIView):
+    serializer_class = SettingUpdateSerializer
+
+    def put(self, request, *args, **kwargs):
+        setting = Setting.objects.first()
+        if not setting:
+            return Response(data={'errors':"No setting to update"}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.update(setting, serializer.validated_data)
+            return Response(status=status.HTTP_200_OK)
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
