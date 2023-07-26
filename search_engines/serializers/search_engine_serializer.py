ef104876c20aae93c4a9c8974e3e21ebe64cd0b5
JohnEf <eustathiou.ioan@protonmail.com>
2023-07-26 21:07:30 +0200

    return search engine name always capitalized

--- search_engines/serializers/search_engine_serializer.py
+++ search_engines/serializers/search_engine_serializer.py
@@ -4,10 +4,17 @@ from search_engines.models import SearchEngine
 
 
 class SearchEngineSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField()
+
+    def get_name(self, search_engine):
+        name = search_engine.name
+        if name:
+            return name.capitalize()
 
     class Meta:
         model = SearchEngine
         fields = [
+            'id',
             'name',
             'url',
             'method',
