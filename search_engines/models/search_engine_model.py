3e0611e5e7a62cca09baf19901483eb49f7dfc77
JohnEf <eustathiou.ioan@protonmail.com>
2023-07-28 11:57:20 +0200

    remove unique attribute from name and url fields in SearchEngine model

--- search_engines/models/search_engine_model.py
+++ search_engines/models/search_engine_model.py
@@ -27,13 +27,11 @@ class SearchEngine(models.Model):
     )
     name = models.CharField(
         max_length=50,
-        unique=True,
         verbose_name="Name",
         help_text="Search engine name e.g. Google"
     )
     url = models.URLField(
         max_length=1000,
-        unique=True,
         verbose_name="Action URL",
         help_text="The url of the search engine plus the value of the action attribute of the form e.g. https://www.google.com<b>/search</b>"
     )
