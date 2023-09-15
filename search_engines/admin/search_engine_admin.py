b3c6b25483b469b81dc52542db2db3cfaab09327
JohnEf <eustathiou.ioan@protonmail.com>
2023-09-15 15:24:26 +0200

    admin: Include 'updated_at' field in SearchEngine admin view.

--- search_engines/admin/search_engine_admin.py
+++ search_engines/admin/search_engine_admin.py
@@ -5,9 +5,10 @@ class SearchEngineAdmin(admin.ModelAdmin):
     fields = [
         'id', 'name', 'url', 'method',
         'name_attribute', 'is_default',
+        'updated_at',
     ]
     readonly_fields = [
-        'id',
+        'id', 'updated_at',
     ]
     list_display = [
         'name', 'url', 'method', 'name_attribute', 'is_default',
