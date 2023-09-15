4c3a4ad51de6279ef5c79b2082dee363c6eaff19
JohnEf <eustathiou.ioan@protonmail.com>
2023-09-15 15:37:41 +0200

    refactor: Add docstrings and comments.Implement logic to ensure that only one search engine is set as default.

--- search_engines/signals/search_engine_signals.py
+++ search_engines/signals/search_engine_signals.py
@@ -8,6 +8,10 @@ from users.models import User
 
 @receiver(post_save, sender=User)
 def add_search_engines(sender=None, **kwargs):
+    """
+    Populates the Search Engines table with all available engines in
+    SEARCH_ENGINES_DATA list, setting Google as the default.
+    """
     if SearchEngine.objects.exists():
         return
 
@@ -19,21 +23,34 @@ def add_search_engines(sender=None, **kwargs):
 
 @receiver([post_save, post_delete], sender=SearchEngine)
 def set_default_search_engine_on_change(sender, **kwargs):
+    """
+    Ensures the presence of at least one search engine and enforces a single default engine.
+    """
     search_engines = SearchEngine.objects.all()
+    # Check if there are no existing SearchEngine objects in the database.
+    # If none exist, re-add all the default search engines.
     if not search_engines:
         add_search_engines()
         return
 
-    default_search_engine = search_engines.filter(is_default=True)
-    if default_search_engine:
+    default_search_engines = search_engines.filter(is_default=True)
+    # Ensure there is only one default engine; keep the most recently set default.
+    if len(default_search_engines) > 1:
+        last_modified_engine = default_search_engines.latest('updated_at')
+        rest_default_engines = default_search_engines.exclude(id=last_modified_engine.id)
+        rest_default_engines.update(is_default=False)
+
+    if default_search_engines:
         return
 
+    # Set the Google engine as the default if no default engine currently exists.
     google = search_engines.filter(name__icontains='google').first()
     if google:
         google.is_default = True
         google.save()
         return
 
+    # Set the first available engine as default if none currently exist and Google is not an option.
     first_search_engine = search_engines.first()
     first_search_engine.is_default = True
     first_search_engine.save()
