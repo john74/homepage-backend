b7984af12ca4f50c08a2b0204940afcb21934b95
JohnEf <eustathiou.ioan@protonmail.com>
2023-10-01 17:40:38 +0200

    refactor the update method in the BookmarkCategorySerializer to retain existing field values if no value is provided in the payload for that field

--- bookmarks/serializers/bookmark_category_serialzer.py
+++ bookmarks/serializers/bookmark_category_serialzer.py
@@ -4,7 +4,6 @@ from bookmarks.models import BookmarkCategory
 
 
 class BookmarkCategorySerializer(serializers.ModelSerializer):
-
     class Meta:
         model = BookmarkCategory
         fields = [
@@ -15,22 +14,8 @@ class BookmarkCategorySerializer(serializers.ModelSerializer):
             'updated_at'
         ]
 
-    def save(self, validated_data):
-        category = super().save(**validated_data)
-        return {
-            "id": category.id,
-            "name": category.name,
-            "color": category.color,
-            "created_at": category.created_at,
-            "updated_at": category.updated_at
-        }
-
-    def update(self, category, validated_data):
-        updated_category = super().update(category, validated_data)
-        return {
-            "id": updated_category.id,
-            "name": updated_category.name,
-            "color": updated_category.color,
-            "created_at": updated_category.created_at,
-            "updated_at": updated_category.updated_at
-        }
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name') or instance.name
+        instance.color = validated_data.get('color') or instance.color
+        instance.save()
+        return instance
