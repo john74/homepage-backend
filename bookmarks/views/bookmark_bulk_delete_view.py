40cdd324843df05b2f20ca9d92706d13d92df6c2
JohnEf <eustathiou.ioan@protonmail.com>
2023-10-12 20:59:40 +0200

    refactor: Include shortcuts in the Response

--- bookmarks/views/bookmark_bulk_delete_view.py
+++ bookmarks/views/bookmark_bulk_delete_view.py
@@ -1,17 +1,15 @@
-from collections import defaultdict
-from django.core.exceptions import ValidationError
-
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from bookmarks.models import Bookmark
-from bookmarks.serializers import BookmarkSerializer
+from bookmarks.serializers import BookmarkSerializer, ShortcutSerializer
 from bookmarks.utils import group_bookmarks
 
 
 class BookmarkBulkDeleteAPIView(APIView):
-    serializer_class = BookmarkSerializer
+    bookmark_serializer = BookmarkSerializer
+    shortcut_serializer = ShortcutSerializer
 
     def delete(self, request, *args, **kwargs):
         bookmark_ids = request.data.get('ids', [])
@@ -23,9 +21,13 @@ class BookmarkBulkDeleteAPIView(APIView):
         bookmarks_to_delete = all_bookmarks.filter(id__in=bookmark_ids)
         bookmarks_to_delete.delete()
 
-        # Exclude the deleted categories from the original queryset and serialize the remaining categories
+        # Exclude the deleted bookmarks from the original queryset
         all_bookmarks = all_bookmarks.exclude(id__in=bookmarks_to_delete.values('id'))
-        serialized_bookmarks = self.serializer_class(all_bookmarks, many=True).data
+        serialized_bookmarks = self.bookmark_serializer(all_bookmarks, many=True).data
         grouped_bookmarks = group_bookmarks(serialized_bookmarks)
-        response_data = {'bookmarks': grouped_bookmarks}
+
+        shortcuts = all_bookmarks.filter(is_shortcut=True)
+        serialized_shortcuts = self.shortcut_serializer(shortcuts, many=True).data
+
+        response_data = {'bookmarks': grouped_bookmarks, 'shortcuts': serialized_shortcuts}
         return Response(data=response_data, status=status.HTTP_200_OK)
