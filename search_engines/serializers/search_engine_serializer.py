0c9782f745c701f1fff1e267eaa49fd9cfd212be
JohnEf <eustathiou.ioan@protonmail.com>
2023-10-21 11:11:33 +0200

    refactor: Remove unnecessary capitalization in SearchEngineSerializer.Search engine names will be displayed as provided

--- search_engines/serializers/search_engine_serializer.py
+++ search_engines/serializers/search_engine_serializer.py
@@ -4,13 +4,6 @@ from search_engines.models import SearchEngine
 
 
 class SearchEngineSerializer(serializers.ModelSerializer):
-    name = serializers.SerializerMethodField()
-
-    def get_name(self, search_engine):
-        name = search_engine.name
-        if name:
-            return name.capitalize()
-
     class Meta:
         model = SearchEngine
         fields = [
