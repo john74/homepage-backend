275caf3bb46ca63f2e113e41843c7742122e45ca
JohnEf <eustathiou.ioan@protonmail.com>
2023-11-03 17:57:30 +0100

    refactor: Use 'system_of_measurement' field from Setting model to determine 'units'

--- settings/utils/get_location_info.py
+++ settings/utils/get_location_info.py
@@ -3,7 +3,7 @@ import httpx
 def get_location_info(setting):
     latitude = setting.latitude
     longitude = setting.longitude
-    units = 'metric' if setting.metric_units else 'imperial'
+    units = setting.system_of_measurement
     appid = setting.open_weather_api_key
 
     url = f'https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&units={units}&appid={appid}'
