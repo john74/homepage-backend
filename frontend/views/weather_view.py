1e544df41d4b70ab186c9599c97a73ea6c9ae1d3
JohnEf <eustathiou.ioan@protonmail.com>
2023-11-03 11:45:18 +0100

    refactor:Replace hardcoded parameter data in 'open_weather_url' with values from the 'Setting' table and include 'units' in 'response_data' in WeatherAPIView

--- frontend/views/weather_view.py
+++ frontend/views/weather_view.py
@@ -3,17 +3,26 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from settings.models import Setting
 from frontend.utils import (
     extract_current_weather_data, extract_extra_weather_data,
     extract_hourly_forecasts, extract_weekly_forecasts,
     group_forecasts_by_day, group_daily_forecasts
 )
-
+from frontend.constants import OPEN_WEATHER_UNITS
 
 class WeatherAPIView(APIView):
 
     def get(self, request, *args, **kwargs):
-        open_weather_url = f'https://api.openweathermap.org/data/2.5/forecast?lat=52.401621&lon=4.932811&units=metric&lang=en&appid=491f8f7afd8e01c68b5459a12db5a7a5'
+        setting = Setting.objects.first()
+        latitude = setting.latitude
+        longitude = setting.longitude
+        units = setting.system_of_measurement
+        api_key = setting.open_weather_api_key
+
+        open_weather_url = f'https://api.openweathermap.org/data/2.5/forecast?' \
+                        f'lat={latitude}&lon={longitude}&units={units}' \
+                        f'&lang=en&appid={api_key}'
         open_weather_response = httpx.get(open_weather_url)
         weatherData =  open_weather_response.json()
 
@@ -24,13 +33,16 @@ class WeatherAPIView(APIView):
         daily_forecasts = group_forecasts_by_day(weekly_forecasts)
         grouped_daily_forecasts = group_daily_forecasts(daily_forecasts)
 
+        units = OPEN_WEATHER_UNITS[setting.system_of_measurement]
+
         response_data = {
+            "units": units,
             "current": current_weather_data,
             "extra_info": extra_info,
             "forecasts": {
                 "hourly": hourly_forecasts,
                 "weekly": grouped_daily_forecasts
-            }
+            },
         }
 
         return Response(data=response_data, status=status.HTTP_200_OK)
