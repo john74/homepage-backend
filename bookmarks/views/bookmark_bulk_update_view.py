42980e8dc950290b41c6b4859373a5eff9bc605b
JohnEf <eustathiou.ioan@protonmail.com>
2023-11-12 20:18:56 +0100

    refactor: Improve success and error messages for clearer communication on bulk updates in BookmarkBulkUpdateAPIView

--- bookmarks/views/bookmark_bulk_update_view.py
+++ bookmarks/views/bookmark_bulk_update_view.py
@@ -1,47 +1,44 @@
-from django.core.exceptions import ValidationError
-
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from base.utils import get_serializer_error
 from bookmarks.models import Bookmark
 from bookmarks.serializers import BookmarkSerializer, ShortcutSerializer
 from bookmarks.utils import group_bookmarks
 
 
 class BookmarkBulkUpdateAPIView(APIView):
-    bookmark_serializer = BookmarkSerializer
-    shortcut_serializer = ShortcutSerializer
+    bookmark_serializer_class = BookmarkSerializer
+    shortcut_serializer_class = ShortcutSerializer
 
     def put(self, request, *args, **kwargs):
         bookmarks = request.data
-        updated_bookmarks = []
-        errors = []
 
         for bookmark in bookmarks:
-            if 'id' not in bookmark or not bookmark['id']:
-                errors.append(f"id not provided for {bookmark}")
-                continue
-
             try:
                 bookmark_obj = Bookmark.objects.get(id=bookmark['id'])
-            except (Bookmark.DoesNotExist, ValidationError):
-                errors.append(f"{bookmark['id']} is not a valid id")
-                continue
+            except (Bookmark.DoesNotExist, KeyError):
+                return Response(data={"error": "Bookmark not found"}, status=status.HTTP_400_BAD_REQUEST)
 
-            serializer = self.bookmark_serializer(instance=bookmark_obj, data=bookmark)
-            if serializer.is_valid():
-                serializer.update(bookmark_obj, serializer.validated_data)
-            else:
-                for field, message in serializer.errors.items():
-                    errors.append(f'{field} {message[0].lower()}')
+            serializer = self.bookmark_serializer_class(instance=bookmark_obj, data=bookmark)
+            if not serializer.is_valid():
+                error = get_serializer_error(serializer.errors)
+                return Response(data={"error": error}, status=status.HTTP_400_BAD_REQUEST)
+
+            serializer.update(bookmark_obj, serializer.validated_data)
 
         all_bookmarks = Bookmark.objects.all()
-        serialized_bookmarks = self.bookmark_serializer(all_bookmarks, many=True).data
+        serialized_bookmarks = self.bookmark_serializer_class(all_bookmarks, many=True).data
         grouped_bookmarks = group_bookmarks(serialized_bookmarks)
 
         shortcuts = all_bookmarks.filter(is_shortcut=True)
-        serialized_shortcuts = self.shortcut_serializer(shortcuts, many=True).data
+        serialized_shortcuts = self.shortcut_serializer_class(shortcuts, many=True).data
 
-        response_data = {'bookmarks': grouped_bookmarks, 'shortcuts': serialized_shortcuts}
+        response_data = {
+            "message": "Bookmark updated successfully.",
+            "bookmarks": grouped_bookmarks,
+            "shortcuts": serialized_shortcuts
+        }
+
         return Response(data=response_data, status=status.HTTP_200_OK)
