755d06fa519268e4207e1e8ff62d51ede8bd3970
JohnEf <eustathiou.ioan@protonmail.com>
2023-11-12 17:25:10 +0100

    refactor: Enhance get_serializer_error to handle dict or list errors and include field name

--- base/utils/get_serializer_error.py
+++ base/utils/get_serializer_error.py
@@ -1,8 +1,15 @@
 def get_serializer_error(serializer_errors):
-    errors = serializer_errors.items()
-    if not errors:
+    if not serializer_errors:
         return
 
+    if isinstance(serializer_errors, dict):
+        errors = serializer_errors.items()
+
+    if isinstance(serializer_errors, list):
+        errors = serializer_errors[0].items()
+
     first_error = next(iter(errors), None)
-    message = str(first_error[1][0])
-    return message.capitalize()
+    field_name = str(first_error[0]).replace('_', ' ').capitalize()
+    field_message = str(first_error[1][0]).capitalize()
+
+    return f"{field_name}: {field_message}"
