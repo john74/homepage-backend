981482393483e5424f940be493bb88275da88954
JohnEf <eustathiou.ioan@protonmail.com>
2023-11-12 20:11:08 +0100

    refactor: Improve success and error messages for clearer communication on bulk updates in BookmarkCategoryBulkUpdateAPIView

--- bookmarks/views/bookmark_category_bulk_update_view.py
+++ bookmarks/views/bookmark_category_bulk_update_view.py
@@ -1,52 +1,41 @@
-from django.core.exceptions import ValidationError
-
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from base.utils import get_serializer_error
 from bookmarks.models import BookmarkCategory
 from bookmarks.serializers import BookmarkCategorySerializer
 from bookmarks.utils import group_bookmark_categories
 
 
 class BookmarkCategoryBulkUpdateAPIView(APIView):
-    serializer_class = BookmarkCategorySerializer
+    bookmark_category_serializer_class = BookmarkCategorySerializer
 
     def put(self, request, *args, **kwargs):
         categories = request.data
-        updated_categories = []
-        errors = []
 
-        for category_data in categories:
-            if 'id' not in category_data or not category_data['id']:
-                errors.append(f"id not provided for {category_data}")
-                continue
-
+        for category in categories:
             try:
-                category_obj = BookmarkCategory.objects.get(id=category_data['id'])
-            except (BookmarkCategory.DoesNotExist, ValidationError):
-                errors.append(f"{category_data['id']} is not a valid id")
-                continue
+                category_obj = BookmarkCategory.objects.get(id=category['id'])
+            except (BookmarkCategory.DoesNotExist, KeyError):
+                return Response(data={"error": "Category not found"}, status=status.HTTP_400_BAD_REQUEST)
 
-            serializer = self.serializer_class(instance=category_obj, data=category_data)
-            if serializer.is_valid():
-                serializer.save()
-                updated_categories.append(serializer.data)
-            else:
-                errors.extend(serializer.errors)
+            serializer = self.bookmark_category_serializer_class(instance=category_obj, data=category)
+            if not serializer.is_valid():
+                error = get_serializer_error(serializer.errors)
+                return Response(data={"error": error}, status=status.HTTP_400_BAD_REQUEST)
+
+            serializer.save()
 
         # Retrieve all categories
         all_categories = BookmarkCategory.objects.all()
-        serialized_categories = self.serializer_class(all_categories, many=True).data
+        serialized_categories = self.bookmark_category_serializer_class(all_categories, many=True).data
         grouped_categories = group_bookmark_categories(serialized_categories)
-        response_data = {'categories': grouped_categories, 'errors': errors}
 
-        if errors:
-            status_code = status.HTTP_400_BAD_REQUEST
-        elif updated_categories:
-            status_code = status.HTTP_200_OK
-        else:
-            status_code = status.HTTP_204_NO_CONTENT
+        response_data = {
+            "message": "Category updated successfully.",
+            "categories": grouped_categories
+        }
 
-        return Response(data=response_data, status=status_code)
+        return Response(data=response_data, status=status.HTTP_200_OK)
 
