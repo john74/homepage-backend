ab1fee38b7f8cceaa03ad01142f052a910cb4cfb
JohnEf <eustathiou.ioan@protonmail.com>
2023-11-13 15:28:53 +0100

    refactor: Improve success and error messages for clearer communication on bulk creation in BookmarkCategoryBulkCreateAPIView

--- bookmarks/views/bookmark_category_bulk_create_view.py
+++ bookmarks/views/bookmark_category_bulk_create_view.py
@@ -2,25 +2,32 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from base.utils import get_serializer_error
 from bookmarks.models import BookmarkCategory
 from bookmarks.serializers import BookmarkCategorySerializer
 from bookmarks.utils import group_bookmark_categories
 
 
 class BookmarkCategoryBulkCreateAPIView(APIView):
-    bookmark_category_serializer = BookmarkCategorySerializer
+    bookmark_category_serializer_class = BookmarkCategorySerializer
 
     def post(self, request, *args, **kwargs):
         categories = request.data
-        serializer = self.bookmark_category_serializer(data=categories, many=True)
+        serializer = self.bookmark_category_serializer_class(data=categories, many=True)
 
         if not serializer.is_valid():
-            return Response(data={}, status=status.HTTP_400_BAD_REQUEST)
+            error = get_serializer_error(serializer.errors)
+            return Response(data={"error": error}, status=status.HTTP_400_BAD_REQUEST)
+
         serializer.save()
 
         all_bookmark_categories = BookmarkCategory.objects.all()
-        serialized_categories = self.bookmark_category_serializer(all_bookmark_categories, many=True).data
+        serialized_categories = self.bookmark_category_serializer_class(all_bookmark_categories, many=True).data
         grouped_categories = group_bookmark_categories(serialized_categories)
 
-        response_data = {'categories': grouped_categories}
+        response_data = {
+            "message": "Bookmark category created successfully.",
+            "categories": grouped_categories
+        }
+
         return Response(data=response_data, status=status.HTTP_200_OK)
