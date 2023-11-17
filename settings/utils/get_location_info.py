92b17251370a7d541872748c4570ba672c754a20
JohnEf <eustathiou.ioan@protonmail.com>
2023-11-17 17:00:42 +0100

    refactor: Refactor the 'get_location_info' utility function to check for the presence of required parameters, including latitude, longitude, units, and appid. If any of these parameters is missing, the function now returns a HTTP_400_BAD_REQUEST status.

--- settings/utils/get_location_info.py
+++ settings/utils/get_location_info.py
@@ -1,10 +1,17 @@
 import httpx
+from rest_framework import status
+from rest_framework.response import Response
+
 
 def get_location_info(setting):
-    latitude = setting.latitude
-    longitude = setting.longitude
-    units = setting.system_of_measurement
-    appid = setting.open_weather_api_key
+    latitude = getattr(setting, "latitude")
+    longitude = getattr(setting, "longitude")
+    units = getattr(setting, "system_of_measurement")
+    appid = getattr(setting, "open_weather_api_key")
 
-    url = f'https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&units={units}&appid={appid}'
+    if not (latitude and longitude and units and appid):
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    base_url = "https://api.openweathermap.org/data/2.5/weather"
+    url = f"{base_url}?lat={latitude}&lon={longitude}&units={units}&appid={appid}"
     return httpx.get(url)
