6f514cf326a6e4f42c1df5ce8324a11e21dc01c9
JohnEf <eustathiou.ioan@protonmail.com>
2023-11-17 17:04:58 +0100

    refactor: Update HomeListAPIView and WeatherAPIView to gracefully handle situations where the response from the OpenWeather API does not have a status code of 200. In such cases, the 'weatherData' field in the response is now set to an empty dictionary.

--- frontend/views/weather_view.py
+++ frontend/views/weather_view.py
@@ -3,13 +3,14 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from settings.models import Setting
+from base.utils import decrypt_data
 from frontend.utils import (
     extract_current_weather_data, extract_extra_weather_data,
     extract_hourly_forecasts, extract_weekly_forecasts,
     group_forecasts_by_day, group_daily_forecasts
 )
 from frontend.constants import OPEN_WEATHER_UNITS
+from settings.models import Setting
 
 class WeatherAPIView(APIView):
 
@@ -18,34 +19,35 @@ class WeatherAPIView(APIView):
         latitude = setting.latitude
         longitude = setting.longitude
         units = setting.system_of_measurement
-        api_key = setting.open_weather_api_key
+        api_key = decrypt_data(setting.open_weather_api_key)
 
         open_weather_url = f'https://api.openweathermap.org/data/2.5/forecast?' \
                         f'lat={latitude}&lon={longitude}&units={units}' \
                         f'&lang=en&appid={api_key}'
         open_weather_response = httpx.get(open_weather_url)
-        weatherData =  open_weather_response.json()
+        weatherData = {}
+        if (open_weather_response.status_code == 200):
+            weatherData =  open_weather_response.json()
+            forecast_type = setting.forecast_type
+            current_weather_data = extract_current_weather_data(weatherData)
+            extra_info = extract_extra_weather_data(weatherData)
+            hourly_forecasts = extract_hourly_forecasts(weatherData)
+            weekly_forecasts = extract_weekly_forecasts(weatherData)
+            daily_forecasts = group_forecasts_by_day(weekly_forecasts)
+            grouped_daily_forecasts = group_daily_forecasts(daily_forecasts)
+            units = OPEN_WEATHER_UNITS[setting.system_of_measurement]
+            weatherData = {
+                "message": "Weather data updated",
+                "forecast_type": forecast_type,
+                "units": units,
+                "current": current_weather_data,
+                "extra_info": extra_info,
+                "forecasts": {
+                    "hourly": hourly_forecasts,
+                    "weekly": grouped_daily_forecasts
+                }
+            }
 
-        forecast_type = setting.forecast_type
-        current_weather_data = extract_current_weather_data(weatherData)
-        extra_info = extract_extra_weather_data(weatherData)
-        hourly_forecasts = extract_hourly_forecasts(weatherData)
-        weekly_forecasts = extract_weekly_forecasts(weatherData)
-        daily_forecasts = group_forecasts_by_day(weekly_forecasts)
-        grouped_daily_forecasts = group_daily_forecasts(daily_forecasts)
-
-        units = OPEN_WEATHER_UNITS[setting.system_of_measurement]
-
-        response_data = {
-            "message": "Weather data updated",
-            "forecast_type": forecast_type,
-            "units": units,
-            "current": current_weather_data,
-            "extra_info": extra_info,
-            "forecasts": {
-                "hourly": hourly_forecasts,
-                "weekly": grouped_daily_forecasts
-            },
-        }
+        response_data = weatherData
 
         return Response(data=response_data, status=status.HTTP_200_OK)
