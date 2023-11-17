95499328704642b49348c0db4581eb9892ba171b
JohnEf <eustathiou.ioan@protonmail.com>
2023-11-17 16:56:04 +0100

    refactor: Refactor populate_setting_fields signal to pre-save execution and encrypt open_weather_api_key.

--- settings/signals/setting_signal.py
+++ settings/signals/setting_signal.py
@@ -1,24 +1,26 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
+from base.utils import encrypt_data
 from settings.models import Setting
 from settings.utils import get_location_info
 
 
-@receiver(post_save, sender=Setting)
+@receiver(pre_save, sender=Setting)
 def populate_setting_fields(sender, **kwargs):
-    setting = Setting.objects.first()
-    response = get_location_info(setting)
+    setting = kwargs["instance"]
 
+    response = get_location_info(setting)
+    open_weather_api_key = getattr(setting, "open_weather_api_key")
+    setting.open_weather_api_key = encrypt_data(open_weather_api_key)
     if response.status_code != 200:
         return
 
     location_info = response.json()
+
     try:
         setting.country = location_info['sys']['country']
         setting.city = location_info['name']
         setting.timezone = location_info['timezone']
     except KeyError:
         return
-
-    setting.save()
