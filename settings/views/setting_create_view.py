015086146f810f26e04bfea997181c74a64a6f9c
JohnEf <eustathiou.ioan@protonmail.com>
2023-11-17 17:08:19 +0100

    refactor: Enhance messaging in SettingCreateAPIView responses.

--- settings/views/setting_create_view.py
+++ settings/views/setting_create_view.py
@@ -2,19 +2,22 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from base.utils import get_serializer_error
 from settings.models import Setting
 from settings.serializers import SettingSerializer
 
 
 class SettingCreateAPIView(APIView):
-    serializer_class = SettingSerializer
+    setting_serializer_class = SettingSerializer
 
     def post(self, request, *args, **kwargs):
         if Setting.objects.exists():
-            return Response(data={'errors':"Setting exists. Cannot add another one"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(data={"errors": "Setting exists. Cannot add another one"}, status=status.HTTP_400_BAD_REQUEST)
 
-        serializer = self.serializer_class(data=request.data)
+        serializer = self.setting_serializer_class(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(status=status.HTTP_200_OK)
-        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(data={"message": "Setting created successfully"}, status=status.HTTP_200_OK)
+
+        error = get_serializer_error(serializer.errors)
+        return Response(data={"error": error}, status=status.HTTP_400_BAD_REQUEST)
