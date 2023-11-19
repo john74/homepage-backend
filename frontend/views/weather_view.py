45187d6a54272e6b9d7e7531a14895c5c2e6c2d1
JohnEf <eustathiou.ioan@protonmail.com>
2023-11-19 15:00:52 +0100

    feat: Introduce a check in the WeatherAPIView to ensure that the weather data is not updated more frequently than the specified interval. If the update interval hasn't been reached, the API view returns an error message indicating the time remaining until the next update is allowed.

--- frontend/views/weather_view.py
+++ frontend/views/weather_view.py
@@ -1,3 +1,6 @@
+from django.utils import timezone
+from datetime import datetime, timedelta
+
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -16,9 +19,30 @@ from settings.models import Setting
 class WeatherAPIView(APIView):
 
     def get(self, request, *args, **kwargs):
-        weather_data = retrieve_weather_data()
+        setting = Setting.objects.first()
+        if not setting:
+            return Response(data={"error": "No weather data available"}, status=status.HTTP_400_BAD_REQUEST)
+
+        last_updated_time = setting.weather_data_last_updated
+        if last_updated_time:
+            current_time = timezone.now()
+            time_passed_since_last_update = current_time - last_updated_time
+            minutes_passed_since_last_update, seconds_passed_since_last_update = divmod(time_passed_since_last_update.total_seconds(), 60)
+
+            update_interval = setting.weather_data_update_interval
+            minutes_until_next_update = int(update_interval - minutes_passed_since_last_update)
+            if minutes_until_next_update >= 0:
+                seconds_until_next_update = int(60 - seconds_passed_since_last_update)
+                time_message = (
+                    f"{minutes_until_next_update} minute{'s' if minutes_until_next_update != 1 else ''}"
+                ) if minutes_until_next_update else (
+                    f"{seconds_until_next_update} second{'s' if seconds_until_next_update != 1 else ''}"
+                )
+                return Response(data={"error": f"Weather data can be updated again in {time_message}."}, status=status.HTTP_400_BAD_REQUEST)
+
+        weather_data = retrieve_weather_data(setting)
         if not weather_data:
-            return Response(data={"error": "No weather data available"}, status=status.HTTP_500_OK)
+            return Response(data={"error": "No weather data available"}, status=status.HTTP_400_BAD_REQUEST)
 
         weather_data["message"] = "Weather data updated";
         return Response(data=weather_data, status=status.HTTP_200_OK)
