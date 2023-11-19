1150a4a8ab8f289ff28c877b5679c06af8717f54
JohnEf <eustathiou.ioan@protonmail.com>
2023-11-19 17:50:35 +0100

    feat: Add user foreign key to BookmarkCategory model.Migrations have been applied to implement the new model.

--- bookmarks/admin/bookmark_category_admin.py
+++ bookmarks/admin/bookmark_category_admin.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 
 class BookmarkCategoryAdmin(admin.ModelAdmin):
     fields = [
-        'id', 'name', 'color',
+        'id', 'name', 'color', 'user',
     ]
     readonly_fields = [
-        'id', 'created_at', 'updated_at'
+        'id', 'user', 'created_at', 'updated_at'
     ]
     list_display = [
         'id', 'name', 'color', 'created_at', 'updated_at'
--- bookmarks/models/bookmark_category_model.py
+++ bookmarks/models/bookmark_category_model.py
@@ -1,5 +1,6 @@
 import uuid
 
+from django.conf import settings
 from django.db import models
 
 
@@ -8,6 +9,11 @@ class BookmarkCategory(models.Model):
         primary_key=True,
         default=uuid.uuid4,
         editable=False
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name="User",
     )
     name = models.CharField(
         max_length=50,
