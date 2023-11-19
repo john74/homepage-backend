564adc92e0d43351053e2832881ecb585de4720f
JohnEf <eustathiou.ioan@protonmail.com>
2023-11-19 19:11:53 +0100

    feat: Add sub_category and user fields to BookmarkAdmin.

--- bookmarks/admin/bookmark_admin.py
+++ bookmarks/admin/bookmark_admin.py
@@ -3,11 +3,11 @@ from django.contrib import admin
 
 class BookmarkAdmin(admin.ModelAdmin):
     fields = [
-        'id', 'category', 'name', 'url', 'icon_url',
+        'id', 'category', 'sub_category', 'user', 'name', 'url', 'icon_url',
         'is_shortcut',
     ]
     readonly_fields = [
-        'id',
+        'id', 'user',
     ]
     list_display = [
         'id', 'name', 'category', 'is_shortcut'
