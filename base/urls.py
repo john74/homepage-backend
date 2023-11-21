af70c4cf8be6fac2c3b59eed71af99c8abdd92d1
JohnEf <eustathiou.ioan@protonmail.com>
2023-11-21 16:49:06 +0100

    Organize URL patterns by creating a dedicated list for endpoints associated with the 'bookmarks' app.

--- base/urls.py
+++ base/urls.py
@@ -4,9 +4,14 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/bookmarks/', include('bookmarks.urls', namespace='bookmarks')),
     path('api/search-engines/', include('search_engines.urls', namespace='search_engines')),
     path('api/settings/', include('settings.urls', namespace='settings')),
     path('api/users/', include('users.urls', namespace='users')),
     path('api/frontend/', include('frontend.urls', namespace='frontend')),
+]
+
+urlpatterns += [
+    path('api/bookmarks/', include('bookmarks.urls.bookmarks', namespace='bookmarks')),
+    path('api/categories/', include('bookmarks.urls.categories', namespace='categories')),
+    path('api/shortcuts/', include('bookmarks.urls.shortcuts', namespace='shortcuts')),
 ]
