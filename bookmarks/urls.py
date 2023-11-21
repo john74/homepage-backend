6dfeb409f021abb3477eb248e20d5850a0ea8cfe
JohnEf <eustathiou.ioan@protonmail.com>
2023-11-21 12:55:30 +0100

    refactor: Change the data type of the 'category_id' path parameter in BookmarkCategoryDetailAPIView from int to str.

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -8,7 +8,7 @@ app_name = 'bookmarks'
 #bookmark category paths
 urlpatterns = [
     path('categories/', views.BookmarkCategoryListAPIView.as_view(), name="list_categories"),
-    path('categories/<int:category_id>/', views.BookmarkCategoryDetailAPIView.as_view(), name="category_detail"),
+    path('categories/<str:category_id>/', views.BookmarkCategoryDetailAPIView.as_view(), name="category_detail"),
     path('bulk-create-categories/', views.BookmarkCategoryBulkCreateAPIView.as_view(), name="bulk_create_categories"),
     path('bulk-delete-categories/', views.BookmarkCategoryBulkDeleteAPIView.as_view(), name="bulk_delete_categories"),
     path('bulk-update-categories/', views.BookmarkCategoryBulkUpdateAPIView.as_view(), name="bulk_update_categories"),
