9e81f830b6717cf5417bca235be31845f1888981
JohnEf <eustathiou.ioan@protonmail.com>
2023-11-21 11:15:26 +0100

    refactor: Utilize user id to fetch all the data associated with the user making the request in BookmarkCategoryBulkDeleteAPIView.

--- bookmarks/views/bookmark_category_bulk_delete_view.py
+++ bookmarks/views/bookmark_category_bulk_delete_view.py
@@ -10,8 +10,8 @@ from bookmarks.utils import group_bookmark_categories
 
 
 class BookmarkCategoryBulkDeleteAPIView(APIView):
-    bookmark_category_serializer = BookmarkCategorySerializer
-    shortcut_serializer = ShortcutSerializer
+    bookmark_category_serializer_class = BookmarkCategorySerializer
+    shortcut_serializer_class = ShortcutSerializer
 
     def delete(self, request, *args, **kwargs):
         category_ids = request.data.get('ids', [])
@@ -19,8 +19,9 @@ class BookmarkCategoryBulkDeleteAPIView(APIView):
         if not category_ids:
             return Response(data={"error": "No bookmark categories found"}, status=status.HTTP_400_BAD_REQUEST)
 
+        user_id = request.user.id
         # Fetch all categories
-        all_categories = BookmarkCategory.objects.all()
+        all_categories = BookmarkCategory.objects.filter(user=user_id)
 
         # Filter categories to be deleted
         try:
@@ -35,11 +36,11 @@ class BookmarkCategoryBulkDeleteAPIView(APIView):
 
         # Exclude categories that need to be deleted from the original 'all_categories' queryset
         all_categories = all_categories.exclude(id__in=categories_to_delete.values('id'))
-        serialized_categories = self.bookmark_category_serializer(all_categories, many=True).data
+        serialized_categories = self.bookmark_category_serializer_class(all_categories, many=True).data
         grouped_categories = group_bookmark_categories(serialized_categories)
 
         all_shortcuts = Bookmark.objects.filter(is_shortcut=True).order_by("created_at")
-        serialized_shortcuts = self.shortcut_serializer(all_shortcuts, many=True).data
+        serialized_shortcuts = self.shortcut_serializer_class(all_shortcuts, many=True).data
 
         response_data = {
             "message": "Bookmark category deleted successfully.",
