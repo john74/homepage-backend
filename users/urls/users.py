fdd8ba8b8bc3f62889bfe4a644fc520902991175
JohnEf <eustathiou.ioan@protonmail.com>
2023-11-28 12:15:22 +0100

    feat: Introduce RefreshTokenAPIView, inheriting from TokenRefreshView, to enhance response data by including the new access token along with its configured lifetime as defined in Simple JWT settings.

--- users/urls/users.py
+++ users/urls/users.py
@@ -1,7 +1,5 @@
 from django.urls import path
 
-from rest_framework_simplejwt.views import TokenRefreshView
-
 from users import views
 
 
@@ -11,5 +9,5 @@ urlpatterns = [
     path("sign-up/", views.SignUpAPIView.as_view(), name="sign_up"),
     path("sign-in/", views.SignInAPIView.as_view(), name="sign_in"),
     path("sign-out/", views.SignOutAPIView.as_view(), name="sign_out"),
-    path("refresh-token/", TokenRefreshView.as_view(), name="refresh_token"),
+    path("refresh-token/", views.RefreshTokenAPIView.as_view(), name="refresh_token"),
 ]
