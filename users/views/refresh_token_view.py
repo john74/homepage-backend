83e1630e22e07aebd252235f7b4d6b097560f053
JohnEf <eustathiou.ioan@protonmail.com>
2024-01-27 19:00:28 +0100

    refactor:Refactor RefreshTokenAPIView to set an http only cookie before returning the response object.

--- users/views/refresh_token_view.py
+++ users/views/refresh_token_view.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 
+from decouple import config
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenRefreshView
@@ -8,9 +9,8 @@ from rest_framework_simplejwt.views import TokenRefreshView
 class RefreshTokenAPIView(TokenRefreshView):
 
     def post(self, request):
-        response = super().post(request)
-        response_data = {
-            "thikee_access_token": response.data["access"],
-            "thikee_access_token_lifetime": settings.SIMPLE_JWT["ACCESS_TOKEN_LIFETIME"],
-        }
-        return Response(data=response_data, status=status.HTTP_200_OK)
+        access_token = super().post(request).data["access"]
+        response = Response()
+        response.set_cookie(key='thikeeAccessToken', value=str(access_token), httponly=True, expires=int(config("ACCESS_TOKEN_LIFETIME")), path='/')
+        response.status = status.HTTP_200_OK
+        return response
