0663f286a0310bff0123f91edf628e170cbee88f
JohnEf <eustathiou.ioan@protonmail.com>
2024-02-05 12:32:55 +0100

    refactor:Refactor group_bookmarks utility function

--- bookmarks/utils/group_bookmarks.py
+++ bookmarks/utils/group_bookmarks.py
@@ -1,10 +1,12 @@
 from collections import defaultdict
 
 
-def group_bookmarks(bookmarks):
-    grouped_bookmarks = defaultdict(list)
-    for bookmark in bookmarks:
-        category = list(bookmark.keys())[0]
-        grouped_bookmarks[category].append(bookmark[category])
+def group_bookmarks(serialized_bookmarks):
+    unique_category_ids = set([str(bookmark["category"]) for bookmark in serialized_bookmarks])
+    grouped_bookmarks = {category_id: [] for category_id in unique_category_ids}
+
+    for bookmark in serialized_bookmarks:
+        category_id = str(bookmark["category"])
+        grouped_bookmarks[category_id].append(bookmark)
 
     return grouped_bookmarks
