69e64cc1fa5d41257e80769ee93423a914ad423a
JohnEf <eustathiou.ioan@protonmail.com>
2024-02-27 11:39:20 +0100

    refactor:Remove shortcuts from the response data

--- bookmarks/views/bookmark_bulk_update_view.py
+++ bookmarks/views/bookmark_bulk_update_view.py
@@ -4,13 +4,11 @@ from rest_framework.views import APIView
 
 from base.utils import get_serializer_error
 from bookmarks.models import Bookmark
-from bookmarks.serializers import BookmarkSerializer, ShortcutSerializer
+from bookmarks.serializers import BookmarkSerializer
 from bookmarks.utils import group_by_category
 
 
 class BookmarkBulkUpdateAPIView(APIView):
-    bookmark_serializer_class = BookmarkSerializer
-    shortcut_serializer_class = ShortcutSerializer
 
     def put(self, request, *args, **kwargs):
         user_id = request.user.id
@@ -23,7 +21,7 @@ class BookmarkBulkUpdateAPIView(APIView):
             except (Bookmark.DoesNotExist, KeyError):
                 return Response(data={"error": "Bookmark not found"}, status=status.HTTP_400_BAD_REQUEST)
 
-            serializer = self.bookmark_serializer_class(instance=bookmark_obj, data=bookmark)
+            serializer = BookmarkSerializer(instance=bookmark_obj, data=bookmark)
             if not serializer.is_valid():
                 error = get_serializer_error(serializer.errors)
                 return Response(data={"error": error}, status=status.HTTP_400_BAD_REQUEST)
@@ -31,16 +29,11 @@ class BookmarkBulkUpdateAPIView(APIView):
             serializer.update(bookmark_obj, serializer.validated_data)
 
         all_bookmarks = Bookmark.objects.filter(user=user_id)
-        serialized_bookmarks = self.bookmark_serializer_class(all_bookmarks, many=True).data
-        grouped_bookmarks = group_by_category(serialized_bookmarks)
-
-        shortcuts = all_bookmarks.filter(is_shortcut=True)
-        serialized_shortcuts = self.shortcut_serializer_class(shortcuts, many=True).data
+        serialized_bookmarks = BookmarkSerializer(all_bookmarks, many=True).data
 
         response_data = {
             "message": "Bookmark updated successfully.",
-            "bookmarks": grouped_bookmarks,
-            "shortcuts": serialized_shortcuts
+            "bookmarks": serialized_bookmarks
         }
 
         return Response(data=response_data, status=status.HTTP_200_OK)
