15b5df912fc34d9b093122bac2715a1a6eeb3095
JohnEf <eustathiou.ioan@protonmail.com>
2024-02-27 14:25:46 +0100

    refactor:Remove shortcuts from response data.Request data now are received as a list rather than a dictionary with key 'ids'.Did minor naming changes.

--- bookmarks/views/bookmark_bulk_delete_view.py
+++ bookmarks/views/bookmark_bulk_delete_view.py
@@ -5,16 +5,14 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from bookmarks.models import Bookmark
-from bookmarks.serializers import BookmarkSerializer, ShortcutSerializer
+from bookmarks.serializers import BookmarkSerializer
 from bookmarks.utils import group_by_category
 
 
 class BookmarkBulkDeleteAPIView(APIView):
-    bookmark_serializer_class = BookmarkSerializer
-    shortcut_serializer_class = ShortcutSerializer
 
     def delete(self, request, *args, **kwargs):
-        bookmark_ids = request.data.get('ids', [])
+        bookmark_ids = request.data
 
         if not bookmark_ids:
             return Response(data={"error": "No bookmark found"}, status=status.HTTP_400_BAD_REQUEST)
@@ -36,16 +34,11 @@ class BookmarkBulkDeleteAPIView(APIView):
 
         # Exclude the deleted bookmarks from the original queryset
         all_bookmarks = all_bookmarks.exclude(id__in=bookmarks_to_delete.values('id'))
-        serialized_bookmarks = self.bookmark_serializer_class(all_bookmarks, many=True).data
-        grouped_bookmarks = group_by_category(serialized_bookmarks)
-
-        shortcuts = all_bookmarks.filter(is_shortcut=True)
-        serialized_shortcuts = self.shortcut_serializer_class(shortcuts, many=True).data
+        serialized_bookmarks = BookmarkSerializer(all_bookmarks, many=True).data
 
         response_data = {
             "message": "Bookmark deleted successfully.",
-            "bookmarks": grouped_bookmarks,
-            "shortcuts": serialized_shortcuts
+            "bookmarks": serialized_bookmarks,
         }
 
         return Response(data=response_data, status=status.HTTP_200_OK)
