84f41c463c04780c1cdcd0e59763fb693f3be1de
JohnEf <eustathiou.ioan@protonmail.com>
2024-03-01 15:07:17 +0100

    feat:Include bookmark sub categories in the response data of UserSettingsListAPIView

--- frontend/views/user_settings_view.py
+++ frontend/views/user_settings_view.py
@@ -3,10 +3,11 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from bookmarks.models import (
-    Bookmark, BookmarkCategory,
+    Bookmark, BookmarkCategory, BookmarkSubCategory,
 )
 from bookmarks.serializers import (
     BookmarkSerializer, BookmarkCategorySerializer,
+    BookmarkSubCategorySerializer,
 )
 from users.models import User
 from users.serializers import UserSerializer
@@ -18,7 +19,7 @@ class UserSettingsListAPIView(APIView):
     def get(self, request, *args, **kwargs):
         user = User.objects.filter(id=request.user.id).first()
         if not user:
-            return Response(data={"error": "No user found"}, status=status.HTTP_200_OK)
+            return Response(data={"error": "No user found"}, status=status.HTTP_400_BAD_REQUEST)
 
         serialized_user = UserSerializer(user).data
         user_id = user.id
@@ -29,10 +30,14 @@ class UserSettingsListAPIView(APIView):
         all_bookmark_categories = BookmarkCategory.objects.filter(user=user_id)
         serialized_categories = BookmarkCategorySerializer(all_bookmark_categories, many=True).data
 
+        all_bookmark_sub_categories = BookmarkSubCategory.objects.filter(user=user_id)
+        serialized_sub_categories = BookmarkSubCategorySerializer(all_bookmark_sub_categories, many=True).data
+
         response_data = {
             "user": serialized_user,
             "bookmarks": serialized_bookmarks,
-            "bookmark_categories": serialized_categories
+            "bookmark_categories": serialized_categories,
+            "bookmark_sub_categories": serialized_sub_categories,
         }
 
         return Response(data=response_data, status=status.HTTP_200_OK)
