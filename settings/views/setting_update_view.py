cab78b5c11d2d77e251aab16554506b441bfc39e
JohnEf <eustathiou.ioan@protonmail.com>
2024-05-03 16:27:29 +0200

    refactor(settings): Remove redundant setting_serializer_class assignment, utilize SettingSerializer directly

--- settings/views/setting_update_view.py
+++ settings/views/setting_update_view.py
@@ -7,7 +7,6 @@ from settings.serializers import SettingSerializer
 
 
 class SettingUpdateAPIView(APIView):
-    setting_serializer_class = SettingSerializer
 
     def put(self, request, *args, **kwargs):
         user_id = request.user.id
@@ -16,12 +15,11 @@ class SettingUpdateAPIView(APIView):
             return Response(data={'errors':"No setting to update"}, status=status.HTTP_400_BAD_REQUEST)
 
         request.data["user"] = user_id
-        serializer = self.setting_serializer_class(data=request.data)
+        serializer = SettingSerializer(data=request.data)
         if serializer.is_valid():
             updated_setting = serializer.update(setting, serializer.validated_data)
-            serialized_setting = self.setting_serializer_class(updated_setting).data
+            serialized_setting = SettingSerializer(updated_setting).data
             response_data = {
-                "message": "Setting updated successfully",
                 "settings": serialized_setting
             }
             return Response(data=response_data, status=status.HTTP_200_OK)
